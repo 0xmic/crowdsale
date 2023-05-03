5b1538378115ccc37884b5cc09a9e1e94fbbe3c6
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-05-03 12:49:04 -0700

    Update App to load accounts, display crowdsale info

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = () => {
   return (
     <Navbar>
       <img src={logo} alt='logo' width='40' height='40' className='d-inline-block align-top mx-3' />
-      <Navbar.Brand href='#'>DELTA Token Crowdsale</Navbar.Brand>
+      <Navbar.Brand href='#'>Delta Token Crowdsale</Navbar.Brand>
     </Navbar>
   )
 }
