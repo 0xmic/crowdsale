6958c654af593b086aae2a2c9bbac8a3ca6d7ea8
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-05-06 15:01:35 -0700

    Update README. Add comments to contracts

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# Alpha Token Crowdsale Application
-This application shows a simple example of a token sale.
+# Alpha (ALPHA) Crowdsale
+This project demonstrates a basic ERC-20 token crowdsale using a Crowdsale smart contract. It includes a smart contract for the ERC-20 token and the Crowdsale contract that manages the token sale. The front-end allows users to interact with the Crowdsale contract, buy tokens using Ether, and check their token balances. Test files and scripts for deployment are included.
+
+## Stack
+Technologies Used:
+* JavaScript
+* React
+* Solidity
+* Bootstrap
+
+Libraries Used:
+* [React-Bootstrap](https://react-bootstrap.github.io/)
+
+Testing Libraries:
+* [React Testing Library](https://testing-library.com/docs/react-testing-library/intro/)
+* [Jest](https://jestjs.io/)
+* [Chai](https://www.chaijs.com/)
+
+Dev Tools:
+* [Hardhat](https://hardhat.org/)
+* [dotenv](https://www.npmjs.com/package/dotenv)
+
+## Local Testing
+To test the Crowdsale locally, run the following:
+```shell
+npx hardhat node
+
+npx hardhat --network localhost scripts/1_deploy_token.js
+
+npx hardhat --network localhost scripts/2_deploy_crowdsale.js
+
+npm run start
+```
+
+## Deployment
+The Crowdsale has been deployed on the Sepolia and Mumbai test networks and is configured to sell an ERC-20 token, Alpha Token (ALPHA). Users can buy ALPHA tokens using Ether at a specified price set by the contract owner. The Crowdsale contract also has a maximum token limit that can be sold during the crowdsale. The contract owner can finalize the crowdsale, which transfers unsold tokens and Ether raised back to the owner.
+
+![Crowdsale](./public/crowdsale.png)
--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.0;
 
 import './Token.sol';
 
+/**
+ * @title Crowdsale
+ * @dev Crowdsale is a contract for managing a token crowdsale.
+ */
 contract Crowdsale {
     address public owner;
     Token public token;
@@ -13,6 +17,12 @@ contract Crowdsale {
     event Buy(uint256 amount, address buyer);
     event Finalize(uint256 tokensSold, uint256 ethRaised);
 
+    /**
+     * @notice Initialize the Crowdsale contract
+     * @param _token Address of the token to be sold
+     * @param _price Price of each token in wei
+     * @param _maxTokens Maximum number of tokens to be sold in the crowdsale
+     */
     constructor(Token _token, uint256 _price, uint256 _maxTokens) {
         owner = msg.sender;
         token = _token;
@@ -25,11 +35,18 @@ contract Crowdsale {
         _;
     }
 
+    /**
+     * @notice Fallback function to buy tokens using Ether directly
+     */
     receive() external payable {
         uint256 amount = msg.value / price;
         buyTokens(amount * 1e18);
     }
 
+    /**
+     * @notice Buy tokens in the crowdsale
+     * @param _amount Number of tokens to buy
+     */
     function buyTokens(uint256 _amount) public payable {
         require(msg.value == (_amount / 1e18) * price);
         require(token.balanceOf(address(this)) >= _amount);
@@ -40,10 +57,17 @@ contract Crowdsale {
         emit Buy(_amount, msg.sender);
     }
 
+    /**
+     * @notice Set the price of tokens in the crowdsale
+     * @param _price New price of each token in wei
+     */
     function setPrice(uint256 _price) public onlyOwner {
         price = _price;
     }
 
+    /**
+     * @notice Finalize the crowdsale, transfer unsold tokens and Ether to the owner
+     */
     function finalize() public onlyOwner {
         require(token.transfer(owner, token.balanceOf(address(this))));
 
